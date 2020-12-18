bba2ba32c1ee901b7d9e146d6fc7181578b733d2
craigjem <74799039+craigjem@users.noreply.github.com>
2020-12-18 11:19:25 +1000

    PJ-2 Update
    Added - JS styling
    Working on the Nav

--- Project 2/js/app.js
+++ Project 2/js/app.js
@@ -17,7 +17,10 @@
  * Define Global Variables
  * 
 */
+const navigationBar = document.querySelector('#navbar__list');
 
+//Styling Navigation tag
+navigationBar.style.cssText = 'color: white; font-size: 1.4em;';
 
 /**
  * End Global Variables
@@ -35,20 +38,39 @@
 
 // build the nav
 
+// select the first (main) heading of the page
+//const mainHeading = document.querySelector('#navbar__list');
+
+// create a brand new <span> element
+//const newSpan = document.createElement('li');
+
+// add text to the element
+//newSpan.textContent = 'Section 1';
+
+// add the <span> element as the last child element of the main heading
+//mainHeading.appendChild(newSpan);
+//document.body.appendChild(mainHeading);
+
+
+//Insert HTML element to build navigation
+const htmlTextToAdd = '<li href="#section1"> ${section1} </li>';
+navigationBar.insertAdjacentHTML ('afterbegin', htmlTextToAdd);
+
+
 // Grab Section 1
-let firstSection = document.getElementById('section1');
+//let firstSection = document.getElementById('section1');
 // Get Section 1 h2 heading
-let firstSectionHeading = document.querySelector('h2').innerHTML;
+//let firstSectionHeading = document.querySelector('h2').innerHTML;
 
 // Create li element
-let menuOptionOne = document.createElement('li');
+//let menuOptionOne = document.createElement('li');
 // link element to li
-menuOptionOne.textContent = firstSectionHeading;
+//menuOptionOne.textContent = firstSectionHeading;
 
 // Set Nav Ul - Li to section 1 heading
-document.getElementById('navbar__list');
-const navHeading = document.querySelector('ul');
-navHeading.appendChild(menuOptionOne);
+//document.getElementById('navbar__list');
+//const navHeading = document.querySelector('ul');
+//navHeading.appendChild(menuOptionOne);
 
 
 //menuOption.textContent = "Menu";
